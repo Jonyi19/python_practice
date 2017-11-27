5340c604aac2cc00275a9042b8aa49385dd2827f
jonny <jonny@jolian.local>
2017-11-27 23:17:10 +0900

    実行はしていない

---
+++ ex3-3.py
@@ -0,0 +1,20 @@
+# コラッツ数列
+def collatz(number):
+    if number % 2 == 0 :
+        number = number / 2
+        print(number)
+        return number
+    elif number % 2 == 1 :
+        number = number * 3 + 1
+        print(number)
+        return number
+    
+print('整数を入力してください'):
+number = int(input())
+print(number)
+while true :
+    number = collatz(number)
+    if number == 1 :
+        break
+
+print('処理が終了しました')
