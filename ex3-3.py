d9b483aa49cacea24221d80d60e5e14b1b6c1dd8
r-kinjo <r_kinjo@dreamarts.co.jp>
2017-11-28 18:11:06 +0900

    回答修正版

--- ex3-3.py
+++ ex3-3.py
@@ -1,22 +1,21 @@
 # コラッツ数列
 def collatz(number):
-    if number % 2 == 0 :
-        number = int(number / 2)
-        print(number)
-        return number
-    elif number % 2 == 1 :
-        number = number * 3 + 1
-        print(number)
-        return number
-    
-print('整数を入力してください')
-try:
-    number = int(input())
-    while True :
-        number = collatz(number)
-        if number == 1 :
-            break
-    print('処理が終了しました')
-except ValueError:
-    print('Errorです。整数の値を入力して下さい')
+    if number % 2 == 0:
+        return int(number / 2)
+    else:
+        return 3 * number + 1
+
+while True:
+  try:
+    print('整数を入力してください：')
+    i = int(input())
+    break
+  except ValueError:
+    print('エラー：整数値を入力してください。')
+
+while i > 1:
+  i = collatz(i)
+  print(i)
+
+print('処理を終了しました')
 
